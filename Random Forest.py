f430b5b1f489021a8fc70fc74c9d96b0a8c5dfd4
LYLJL <1438932562@qq.com>
2025-12-31 21:16:35 +0800

    Add files via upload

---
+++ Random Forest.py
@@ -0,0 +1,10 @@
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.pipeline import Pipeline
+plt.rcParams['font.family'] = 'sans-serif'
+plt.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
